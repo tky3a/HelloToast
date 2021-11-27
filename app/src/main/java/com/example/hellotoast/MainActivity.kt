90e5e2b2f9d87129a197ad1fb5fdd357093781fb
tky3a <takayamac@icloud.com>
2021-11-27 20:24:19 +0900

    サイコロ２個を表示してサイコロを回すロジックを追加

--- app/src/main/java/com/example/hellotoast/MainActivity.kt
+++ app/src/main/java/com/example/hellotoast/MainActivity.kt
@@ -2,39 +2,59 @@ package com.example.hellotoast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    // ※初期化遅延 lateinitで初期化するタイミングを指定できる（null許容として扱える）
+    // 自動的に初回はnullになる
+    private lateinit var diceImage : ImageView
+    private lateinit var diceImage2 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // ROLLボタン押下
         val rollButton : Button = findViewById(R.id.btn_roll)
-        val resultText : TextView = findViewById(R.id.textView2)
-        val btnCntUp : Button = findViewById(R.id.btnCntUp)
-        btnCntUp.isEnabled = false
         rollButton.setOnClickListener {
-            val randomInt = (1..6).random()
-            resultText.text = randomInt.toString()
-            // ROLLがタップされたタイミングでtrue
-            btnCntUp.isEnabled = true
             rollDice()
         }
-        btnCntUp.setOnClickListener {
-            val currentText = resultText.text.toString().toIntOrNull()
-            // nullではなく、int型の時に発火
-            if (currentText != null) {
-                var num = resultText.text.toString().toInt()
-                num++
-                resultText.text = num.toString()
-            }
+
+        // ※ここで初期化を行う！view取得
+        diceImage = findViewById(R.id.diceImage1)
+        diceImage2 = findViewById(R.id.diceImage2)
+        // クリアボタンのView取得
+        val clearBtn: Button = findViewById(R.id.btnClear)
+        // クリアボタン押下
+        clearBtn.setOnClickListener {
+            diceClear(diceImage, diceImage2)
         }
     }
 
+    // Rollボタン発火関数
     private fun rollDice() {
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        // ２つのダイスを回す
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage() : Int {
+        val randomInt = (1..6).random()
+
+        // whenで画像データをランダムで設定
+        return when (randomInt) {
+            1 -> R.drawable.dice_1 // R.drawableでdrawableディレクトリを参照している
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
+
+    private fun diceClear(diceImage:ImageView,diceImage2:ImageView ) {
+        diceImage.setImageResource(R.drawable.empty_dice)
+        diceImage2.setImageResource(R.drawable.empty_dice)
     }
 }
